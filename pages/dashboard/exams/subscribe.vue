b70790a028660b98101692a0422df5547f26ec38
Ahamba Solomon <ahamba.solomon@princepsfinance.com>
2021-02-19 19:57:40 +0100

    sid,s smdcds

--- pages/dashboard/exams/subscribe.vue
+++ pages/dashboard/exams/subscribe.vue
@@ -43,12 +43,30 @@
                 </div>
               </div>
 
+              <div class="my-3 row">
+                <div class="single-content p-2">
+                  <exambox></exambox>
+                  <div class="price-content selected mx-2">
+                    <div class="row mx-2">
+                      <div class="col-md-3 price_1 priceblock">
+                        <span>Subscription fee</span> <br>
+                        <span class="bold1"> 100000 </span>
+                      </div>
+                      <div class="col-md-3 ml-1 price_2 priceblock">
+                        <span> Total </span> <br>
+                        <span class="bold1"> 100000 </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
               <div class="row my-3">
                 <div class="col-md-2">
-                  <b-icon icon="arrow-left-square nav"></b-icon>
+                  <b-icon icon="arrow-left-square" class="float-left" scale="2" ></b-icon>
                 </div>
                 <div class="offset-md-8 col-md-2">
-                  <b-icon icon="arrow-right-square nav"></b-icon>
+                  <b-icon icon="arrow-right-square" class="float-right" scale="2"></b-icon>
                 </div>
               </div>
 
@@ -60,6 +78,51 @@
   </div>
 </template>
 <style lang="scss" scoped>
+  .selected {
+    border: 1px solid #AAB750;
+    box-sizing: border-box;
+    background: white;
+    border-radius: 5px;
+  }
+  .bold1 {
+    font-weight: bold;
+    // color: $prmary;
+  }
+  .price_1 {
+    background: #FCFFEA;
+    border: 1px solid #E0E0E0;
+    span {
+      color: #333333;
+      font-size: 14px;
+      line-height: 140%;
+      /* identical to box height, or 20px */
+      text-align: center;
+    }
+  }
+
+  .price_2 {
+    background: #8369AC;
+    border-radius: 5px;
+    span {
+      color: #FFFFFF;
+      font-size: 14px;
+      line-height: 140%;
+      /* identical to box height, or 20px */
+      text-align: center;
+    }
+  }
+
+  .priceblock {
+    margin-top: 3px;
+    height: 80px;
+    padding-top: 3%;
+    text-align: center;
+    box-sizing: border-box;
+    border-radius: 5px;
+  }
+  .price-content {
+    height: 88px;
+  }
   .cancel {
     font-size: 14px;
     line-height: 16px;
@@ -81,6 +144,10 @@
   .list-content {
     height: 500px;
     overflow-y: scroll;
+    background: #FFFFFF;
+  }
+  .single-content {
+    height: 220px;
     background: #FFFFFF;
   }
 
